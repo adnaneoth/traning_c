a7c9ac9c892d513dcd4effc721ab5f75d9616d5f
adnaneoth <adnaneothmane64@gmail.com>
2023-09-14 15:36:30 +0200

    calcul if else

--- test2/main.c
+++ test2/main.c
@@ -3,38 +3,9 @@
 
 int main()
 {
- int x,y,r,c;
+
 /*
-
-    float y=7.5;
-
-    char nom []="othmane";
-
-
-
-    printf("hello fati \n");
-    printf("hello adnane\n");
-    printf(" x = %d et y = %.2f",x,y);
-    printf(" \n my name is %s",nom);
-
-
-
-
-
-   printf("donnez moi un nombre");
-
-   scanf("%d",&x);
-    printf("\ndonnez moi un nombre");
-
-   scanf("%d",&y);
-
-   r=x+y;
-
-   printf("\n la somme est %d",r);
-
-*/
-
-
+int x,y,r,c;
     printf(" 1_addition \n 2- soustraction\n 3_division \n 4_multiplication ");
 
     scanf("%d",&c);
@@ -95,12 +66,53 @@ break;
 
 break;
 
+ };
+*/
+
+
+
+int x,y,i=0;
+float r;
+ char c;
+printf("calculatrice\n");
+while(i==0){
+
+
+scanf("\n%d %c %d",&x,&c,&y);
+
+if(c=='+')
+{
+    r=x+y;
+    printf("\n%d %c %d = %.2f",x,c,y,r);
+}
+
+else if(c=='-')
+{
+    r=x-y;
+    printf("\n%d %c %d = %.2f",x,c,y,r);
+}
+
+else if(c=='/')
+{
+    r=x/y;
+    printf("\n%d %c %d = %.2f",x,c,y,r);
+}
+
+
+else if(c=='*')
+{
+    r=x*y;
+    printf("\n%d %c %d = %.2f",x,c,y,r);
+}
+
+else
+    printf("invalide\n");
 
 
 
 
+}
 
-    };
 
 
 
